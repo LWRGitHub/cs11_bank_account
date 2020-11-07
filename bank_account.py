417b07909a6ebcc310fe8d29162d529df2f143da
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-11-06 17:31:43 -0800

    init in frist class done

--- bank_account.py
+++ bank_account.py
@@ -1,4 +1,9 @@
 
 Class Bank_Account:
     def __init__(self, full_name, account_number, routing_number, balance):
-        
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
+
+    
